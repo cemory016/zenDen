ece05a658eed08217751721bdc7fa20f9ea4dc54
Carmen <cemory016@gmail.com>
2018-02-22 05:40:02 -0500

    basic server set up

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 //this is the app for the zenDen
+
+var express = require('express');
+var bodyParser = require('body-parser');
+//var session = require('express-session');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var hbs = require('hbs')
+var mongoose = require('mongoose');
+
+var usersController = require('./controllers/users.js');
+//var sessionsController = require('./controllers/sessions.js');
+const itemsController = require('./controllers/itemsController.js')
+app.use('/users/:userId/items', itemsController)
+var app = express();
+
+// ADD THE NAME OF YOUR DATABASE
+// mongoose.connect('mongodb://localhost/<YourDatabaseNameHere>');
+
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger('dev'));
+app.use(methodOverride('_method'));
+
+// app.use(session({
+//   secret: "derpderpderpcats",
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+app.use('/users', usersController);
+//app.use('/sessions', sessionsController);
+
+// Will log an error if db can't connect to MongoDB
+db.on('error', function (err) {
+    console.log(err)
+  })
+  
+  // Will log "database has been connected" if it successfully connects.
+  db.once('open', function () {
+    console.log('database has been connected!')
+  })
+
+app.listen(4000);
