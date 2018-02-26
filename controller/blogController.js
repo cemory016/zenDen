ce8b70afb941ed1fcc3f745044235051cbea833a
Carmen <cemory016@gmail.com>
2018-02-26 02:14:31 -0500

    blog is populating but some post for all users, need a fix

--- controller/blogController.js
+++ controller/blogController.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
         res.render('users/blog/index', {
             user: user,
             blogs: blogs,
+            id: req.params.usersId,
         })
     })
   
@@ -26,7 +27,10 @@ router.get('/', (req, res) => {
   
   //USER NEW------GET//
   router.get('/new', (req, res) => {
-  res.render('users/blog/new')
+    
+  res.render('users/blog/new', {
+    id: req.params.usersId,
+  })
   })
   
   //USER SHOW-----GET//
@@ -35,7 +39,7 @@ router.get('/', (req, res) => {
         const blog = user.blog.id(req.params.id)
         res.render('users/blog/show', {
             blogID: req.params.id,
-            id: req.params.usersID,
+            id: req.params.usersId,
             user: user,
             blog: blog, 
         })
@@ -44,19 +48,19 @@ router.get('/', (req, res) => {
   })
   
   
-  // //USER PATCH------PUT/UPDATE//
-  // router.patch('/user/:id/blog', (req, res) => {
-  //   blog.findByIdAndUpdate(req.params.id, {
-  //       title: req.body.title,
-  //       words: req.body.words,
-  //   }, {new: true}).then((updatedBlog) => {
-  //       console.log(updatedBlog);
-  //       res.redirect(`blog/users/${updatedUser.id}`)
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   })
-  // })
+  //USER PATCH------PUT/UPDATE//
+  router.patch('/:id/blogs', (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        words: req.body.words,
+    }, {new: true}).then((updatedBlog) => {
+        console.log(updatedBlog);
+        res.redirect(`users/:id/blogs/${updatedBlog.id}`)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
   
   // router.delete('blog/:id', (req, res) => {
   //   console.log("trying to delete");
@@ -70,17 +74,18 @@ router.get('/', (req, res) => {
   // })
   
   // //USER------CREATE/POST//
-  // router.post('/', (req, res) => {
-  // const newBlog = new Blog({
-  //   username: req.body.name,
-  //   email: req.body.email,
-  //   profile: req.body.profile,
-  // })
-  
-  // newUser.save().then((savedUser) => {
-  //   res.redirect(`/users/${savedUser._id}`)
-  // })
-  // })
+  router.post('/', (req, res) => {
+  User.findById(req.params.usersId).then((user) => {
+      const newBlog = new Blog({ 
+          title: req.body.title,
+          words: req.body.words,
+      })
+      user.blog.push(newBlog)
+      return user.save()
+    }).then((updatedUser) => {
+        res.redirect(`/users/${req.params.usersId}/blogs`)
+    })
+  })
   
   //USER EDIT-----GET//
   router.get('/:id/edit', (req, res) => {
