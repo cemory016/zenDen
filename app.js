651b0208d4d80e159c44137c8b4fd56f67bbead9
Carmen <cemory016@gmail.com>
2018-02-28 07:56:59 -0500

    still nothing...

--- app.js
+++ app.js
@@ -60,11 +60,11 @@ app.use('/users/:usersId/moodGoal', moodGoals);
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function (err, req, res, next) {
